636ebb29cbee3e078f0a44e5e3313e5a8ef3a694
jamesprices <james@jamesmprice.com>
2015-04-21 21:57:13 -0600

    Lots of stuff
    Added a ton of logic

--- js/match.js
+++ js/match.js
@@ -1,4 +1,55 @@
 $(function() {
+
+  var picList =   [
+                        {word: 'apple', file: 'apple1.jpg'},
+                        {word: 'ball', file: 'ball.jpg'},
+                        {word: 'cat', file: 'cat1.jpg'},
+                        {word: 'cheese', file: 'cheese.jpg'},
+                        {word: 'cloud', file: 'cloud1.jpg'},
+                        {word: 'cow', file: 'cow1.jpg'},
+                        {word: 'dog', file: 'dog1.png'},
+                        {word: 'grass', file: 'grass.jpg'},
+                        {word: 'hash', file: 'hash.jpg'},
+                        {word: 'pepper', file: 'pepper.jpg'},
+                        {word: 'rainbow', file: 'rainbow1.png'},
+                        {word: 'salt', file: 'salt.jpg'},
+                        {word: 'worm', file: 'worm.jpg'},
+                    ]
+    // Generate the page
+    // Choose a random picture as an answer
+    var generateGame = function() {
+      $('#picture-div').empty();
+      options = Array();
+      var answer = picList[Math.floor(Math.random() * picList.length)];
+      options.push(answer);
+
+      // Choose two more as options
+      var numOptions=2;
+      var renderOptions = function(numOptions){
+        for(i=0; i<numOptions;){
+          var random = picList[Math.floor(Math.random() * picList.length)];
+          if($.inArray(random,options)===-1){
+             options.push(random);
+          }
+          if(options.length!==numOptions-1){
+            i++;
+          }
+        }  
+       
+      }
+      renderOptions(numOptions);
+      // Shuffle!
+      options = shuffle(options);
+
+
+      for(i=0; i<options.length; i++){
+        item=i+1;
+        $('#picture-div').append('<img id="'+options[i].word+'" class="drag" src="images/'+options[i].file+'">')
+      }
+    }
+    generateGame();
+
+    // Move and validate stuff
     $( ".drag" ).draggable({ revert: "invalid" });
     $( "#dropzone" ).droppable({
       drop: function( event, ui ) {
@@ -17,21 +68,7 @@ $(function() {
        
     }
 
-    var picList =   [
-                        {word: 'apple', file: 'apple1.jpg'},
-                        {word: 'ball', file: 'ball.jpg'},
-                        {word: 'cat', file: 'cat1.jpg'},
-                        {word: 'cheese', file: 'cheese.jpg'},
-                        {word: 'cloud', file: 'chris.jpg'},
-                        {word: 'cow', file: 'cow1.jpg'},
-                        {word: 'dog', file: 'dog1.jpg'},
-                        {word: 'grass', file: 'grass.jpg'},
-                        {word: 'hash', file: 'hash.jpg'},
-                        {word: 'pepper', file: 'pepper.jpg'},
-                        {word: 'rainbow', file: 'rainbow.jpg'},
-                        {word: 'salt', file: 'salt.jpg'},
-                        {word: 'worm', file: 'worm.jpg'},
-                    ]
+    
 
 });
 
